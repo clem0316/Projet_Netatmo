067da0de910537c706e6f11c31d1bcd958332446
Clément Badin <clement@macbook-air-de-clement.home>
2023-10-08 10:10:40 +0200

    [FIX PROBLEM] with last commit

--- shadows_and_silhouettes/src/components/Cards.jsx
+++ shadows_and_silhouettes/src/components/Cards.jsx
@@ -57,7 +57,7 @@ export default function Cards() {
         ]
       );
     }
-    console.log(theUsers);
+    console.log(theUsers.filter((user) => user.gender === "male"));
 
     userContent = (
       <ul>
